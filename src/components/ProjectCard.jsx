353bc690274160f109f2966aa347321aecd07f68
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-19 07:50:42 +0545

    made the table responsive

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -18,7 +18,7 @@ const ProjectCard = () => {
   };
 
   return (
-    <div className="relative max-w-[840px] max-md:max-w-[600px] max-sm:max-w-[380px] mx-auto">
+    <div className="relative max-w-[840px] max-md:max-w-[600px] max-sm:max-w-[340px] mx-auto">
       <div
         className="flex overflow-x-auto space-x-4 px-6 scroll-smooth custom-scrollbar"
         ref={scrollContainerRef}
